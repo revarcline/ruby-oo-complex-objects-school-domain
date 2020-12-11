09a6f84d4d66a2ff5571e705694a7cf670ab5d25
Alex Cline <rev.a.r.cline@gmail.com>
2020-12-11 14:35:18 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+# school daze babey
+
+# keep student roster and school name
+class School
+  attr_accessor :school_name, :roster
+
+  def initialize(school_name)
+    @roster = {}
+    @school_name = school_name
+  end
+
+  def add_student(name, grade)
+    @roster[grade] = [] unless @roster[grade]
+    @roster[grade] << name
+  end
+
+  def grade(year)
+    @roster[year]
+  end
+
+  def sort
+    @roster.transform_values { |value| value.sort { |a, b| a <=> b } }
+  end
+end
